adcbb295c36b2c0b043c80b1a72146dfac949fc9
GeraltV8 <geraquesearock@gmail.com>
2025-06-04 19:48:50 -0300

    added spinner with coroutines

--- app/src/main/java/com/profegera/dogapi/restclient/RetrofitClient.kt
+++ app/src/main/java/com/profegera/dogapi/restclient/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "https://dog.ceo/api/"
-
+//hola
     val instance: DogApi by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
